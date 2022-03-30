e82fbd4f1f8ff938786d9314757b38de865f5144
Uardet99 <eduardvalls17@gmail.com>
2022-03-30 23:42:55 +0200

    Optimizando el condiciona _POST

--- Formularios/recibe.php
+++ Formularios/recibe.php
@@ -6,7 +6,7 @@
 
 # Este condicional lo que hace es: Si hay _POST, es decir, si contiene informacion muestranoasla, si no (else)
 # redirigenos a la pagina de formulario
-if ($_POST) {
+/* if ($_POST) {
 
     # De esta forma lo que hemos hecho es alamacenar mediante el metodo _POST guardar la informacion en variables
     $nombre = $_POST['nombre'];
@@ -18,7 +18,21 @@ if ($_POST) {
 } else {
     # El header sirve para redirigirnos
     header('Location: http://localhost/curso_php/Formularios/');
+} */
+
+
+# Optimizando el condicional
+
+# Si no hay _POST me vas a ejecutar nuestra redireccion
+if (!$_POST) {
+    header('Location: http://localhost/curso_php/Formularios/');
 }
 
+$nombre = $_POST['nombre'];
+$sexo = $_POST['sexo'];
+$year = $_POST['year'];
+$terminos = $_POST['terminos'];
+
+echo 'Hola, ' . $nombre . ' eres ' . $sexo;
 
 ?>
